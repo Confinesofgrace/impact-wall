7341773f8c152fb787210e20db2d591320589495
Damilola Akinlolu <confinesofgrace@gmail.com>
2024-11-29 10:25:31 +0100

    Another Layout

--- src/Pages/CohortParticipants.jsx
+++ src/Pages/CohortParticipants.jsx
@@ -3,28 +3,19 @@ import './CohortParticipants.css'
 function CohortParticipants () {
     return(
         <div id="cohort-pageLayout">
-            <div id="sidebar">for the sidebar</div>
+            
 
-            <div id="content">
+            <div id="cohort-album">
                 <div id="content-top">
                 <p style={{
                         fontSize:'24px',
                         fontWeight:'600',
-                    }}>Participants List</p>
-                    <input placeholder='Search participants' 
-                    style={{
-                        width:'60%',
-                        height:'48px',
-                        padding:'10px',
-                        margin:'10px 0px',
-                        borderStyle:'none',
-                        border:'1px solid',
-                        borderRadius:'12px'
-                    }}/>
+                        marginBottom:'10px'
+                    }}>2024 Autumn Cohort</p>
+                    
                 </div>
 
                 <div id="participant-list">
-                    <p>2024 Autumn Cohort</p>
 
                     <div style={{display:'flex', gap:'10px', marginBottom:'10px',}}>
 
